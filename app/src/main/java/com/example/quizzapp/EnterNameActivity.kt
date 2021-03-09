f54685fef900b75f0511e621cdfbe5c6d9162450
noah-erasmus <55457778+noah-erasmus@users.noreply.github.com>
2021-03-09 15:33:09 +0200

    Initial commit
    Include basic designs

---
+++ app/src/main/java/com/example/quizzapp/EnterNameActivity.kt
@@ -0,0 +1,11 @@
+package com.example.quizzapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EnterNameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_enter_name)
+    }
+}
